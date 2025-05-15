b907d6dc1cd5376645e3ef70c181ca7265709d92
Migueeel16 <48095097g@iespoblenou.org>
2025-05-15 18:53:13 +0200

    ya funcionaaaa

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import entities.Alumne;
+import providers.DataProvider;
+import providers.DataProviderFactory;
+
 public class Main {
     public static void main(String[] args) {
-
+        DataProvider dataProvider = DataProviderFactory.createInstance();
+        Alumne alumne = dataProvider.find(Alumne.class, 10);
+        System.out.println(alumne);
     }
 }
